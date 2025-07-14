57e3c5c47a73b0779df5db8decd8f9a2c5a58a4e
Virendra-007 <virendralokhande007@gmail.com>
2025-07-15 00:37:06 +0530

    Update Main.java

--- day 2/src/Main.java
+++ day 2/src/Main.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
-
+/* 
+asking user to input the marks that he scored and with that i 
+calculate his percentage behalf of the max marks given.
+    */
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
