02214e20e006e682d04fa80d7e6c143a1961b664
Virendra-007 <virendralokhande007@gmail.com>
2025-07-17 00:55:40 +0530

    Add files via upload
    Added Day 4: Continued Java methods with simple function

---
+++ day 4/src/methods.java
@@ -0,0 +1,10 @@
+public class methods {
+    public static int square(int num) {
+        return num * num;
+    }
+
+    public static void main(String[] args) {
+        int result = square(5);
+        System.out.println("Square of 5 is: " + result);
+    }
+}
