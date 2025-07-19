ab612816c4a394a8c5975b2d284af146f7a9376c
Virendra Lokhande <virendralokhande007@gmail.com>
2025-07-19 23:36:54 +0530

    day 6 -Starting the SQL part.
    Topics Covered:
    - What is SQL and why it's used
    - Basic SQL syntax and structure
    - Introduction to Databases, Tables, and Records
    - Learned to create a small sample database
    - Practiced basic SQL commands

---
+++ firstSQL.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Students (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT,
+    grade VARCHAR(5)
+);
+
+INSERT INTO Students VALUES (1, 'Virendra', 20, 'A');
+SELECT * FROM Students;
